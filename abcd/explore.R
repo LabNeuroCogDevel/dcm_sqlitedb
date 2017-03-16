cb184ae9cac396656098a40910d79852a1548f97
WillForan <will.foran@gmail.com>
2017-03-16 12:06:36 -0400

    add task count to summary

--- abcd/explore.R
+++ abcd/explore.R
@@ -8,38 +8,45 @@ showtime <- function(x)   strftime(x,'%H:%M',tz="UTC")
 
 db <- src_sqlite('db.sqlite3')
 #d<-tbl(db,sql("select scanid,ndcm,seqno,Name,Date from mrinfo where Name like '%fMRI_rest%'"))
-m<-as.data.frame(tbl(db,sql("select v.id,Name,mot.value as motion,er.value as extrisk, mint,maxt,finaldur,Date,Time,ageatscan as age,Sex,Operator,seqno,ndcm,RT from mrinfo m join visit v on m.scanid=v.scanid left join motion mot on mot.id=v.id left join extrisk er on er.id=v.id")))
+m<-as.data.frame(tbl(db,sql("select v.id,Name,mot.value as noMotion,er.value as extrisk, mint,maxt,finaldur,Date,Time,ageatscan as age,Sex,Operator,seqno,ndcm,RT from mrinfo m join visit v on m.scanid=v.scanid left join motion mot on mot.id=v.id left join extrisk er on er.id=v.id")))
 
 m$dt <- datetime(m$Date,m$Time)
 
 smry <- 
   m %>% 
-  group_by(id,Date,Operator,Sex,motion,age,extrisk) %>% 
+  group_by(id,Date,Operator,Sex,noMotion,age,extrisk) %>% 
   arrange(dt) %>%
   summarise(
-    firstSeqStartT=min(dt),
-    lastSeqEndT=last(dt)+last(finaldur),
-    durMin=(lastSeqEndT-firstSeqStartT)/60,
+    startTime=min(dt),
+    endTime=last(dt)+last(finaldur),
+    durMin=round((endTime-startTime)/60,2),
     nAllSeq=n(),
-    nRestSeqDcm383=length(which(ndcm==383 & grepl('ABCD_fMRI_rest',Name))),
+    nTask=length(which( Name %in% c('ABCD_fMRI_task_Emotional_n-back','ABCD_fMRI_task_Monetary_Incentive','ABCD_fMRI_task_Stop'))),
+    nTaskComp=length(which(
+      (Name=='ABCD_fMRI_task_Emotional_n-back'   & ndcm==370) |
+      (Name=='ABCD_fMRI_task_Monetary_Incentive' & ndcm==411) |
+      (Name=='ABCD_fMRI_task_Stop'               & ndcm==445)
+    )),
+    nRestComp=length(which(ndcm==383 & grepl('ABCD_fMRI_rest',Name))),
     nRest=length(which(grepl('ABCD_fMRI_rest',Name))),
     nRestDcm=sum(ndcm[grepl('ABCD_fMRI_rest',Name)])
   ) %>% 
   mutate(
-    firstSeqStartT=showtime(firstSeqStartT),
-    lastSeqEndT=showtime( lastSeqEndT)
+    startTime=showtime(startTime),
+    endTime=showtime( endTime)
   )
 
 write.table(smry,file='out/summary.csv',row.names=F,sep=",")
 
 p <- ggplot(smry) + 
   aes(
-    y=nRestSeqDcm383,
-    x=as.factor(extrisk),
+    x=noMotion,
+    y=as.factor(extrisk),
     color=age,
     size=durMin
   ) +
-  geom_jitter(width=.2,height=.2)+
+  #geom_jitter(width=.2,height=.2)+
+  geom_point() +
   theme_bw()
 print(p)
 
